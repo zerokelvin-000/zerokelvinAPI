9a4fd5f5af51958da69eb91832942cdb187fd6ab
zerokelvin000 <zerokelvin.personal@gmail.com>
2024-12-22 10:55:11 +0100

    finita l'implementazione di javascript

--- examples/JavaScript/uso delle credenziali/configs.js
+++ examples/JavaScript/uso delle credenziali/configs.js
@@ -1,1 +1,12 @@
-// TODO
+class Configs{
+    set_token(token){
+        this.token = token;
+    }
+
+    get_token(){
+        return this.token;
+    }
+}
+
+const configs = new Configs;
+configs.set_token("token_di_prova");
